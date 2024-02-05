b5e056370998203242179bf5bbef3cb601630a5c
Teddy Peterschmidt <t.a.peterschmidt@gmail.com>
2024-02-05 14:20:36 -0800

    add ability to add playlist to song

--- PlaylistManager/Controllers/SongsController.cs
+++ PlaylistManager/Controllers/SongsController.cs
@@ -36,5 +36,24 @@ namespace PlaylistManager.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult AddPlaylist(int id)
+        {
+            Song song = _db.Songs.FirstOrDefault(song => song.SongId == id);
+            ViewBag.PlaylistId = new SelectList(_db.Playlists, "PlaylistId", "Name");
+            return View(song);
+        }
+
+        [HttpPost]
+        public ActionResult AddPlaylist(Song song, int playlistId)
+        {
+            PlaylistSong? joinEntity = _db.PlaylistSongs.FirstOrDefault(join => (join.PlaylistId == playlistId && join.SongId == song.SongId));
+            if (joinEntity == null && playlistId != 0)
+            {
+                _db.PlaylistSongs.Add(new PlaylistSong() { PlaylistId = playlistId, SongId = song.SongId });
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Details", new { id = song.SongId });
+        }
     }
 }
